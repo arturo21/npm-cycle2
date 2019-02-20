a4f821828d562319c8ae98e6d31034b4828cb137
Arturo Vasquez <artav17@gmail.com>
2019-02-20 17:02:26 -0400

    cambios

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-require("./tinyslider/dist/tiny-slider.css");
-require("./tinyslider/dist/tiny-slider.helper.ie8.js");
-require("./tinyslider/dist/tiny-slider.js");
-require("./gcycle.js");
+require("./dist/cycle2.js");
